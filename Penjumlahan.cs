960246711bcbb3f3ffe7a0ebda61e4ca42d11af3
Mochamad Aqsa <101115008+xaqsox@users.noreply.github.com>
2023-03-20 08:00:12 +0700

    menambahkan class SimpleDataBase
    menambahkan class SimpleDataBase

---
+++ Penjumlahan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302190044
+{
+   class Penjumlahan
+    {
+        public void JumlahTigaAngka<T>(T angka)
+        {
+            Console.WriteLine(angka);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul5_1302190044;
+using System;
+using System.Collections.Generic;
+
+using System.Threading.Tasks;
+
+namespace tpmodul5_1302190044
+{
+    class Program
+    {
+
+        class SimpleDataBase<T> : List<DateTime>
+        {
+            public SimpleDataBase(){ }
+          
+            void AddNewData(T StoredData, DateTime inputDates)
+            {
+                inputDates = DateTime.Now;
+            }
+            void PrintAllData()
+            {
+
+            }
+            static void Main(string[] args)
+            {
+                Penjumlahan jml = new Penjumlahan();
+               // PrintAllData();
+            }
+        }
+    }
+}
